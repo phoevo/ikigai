7ed2ee9ce4898a33883a18e066422ef47b2cf42f
Phoevo <148871904+phoevo@users.noreply.github.com>
2025-05-01 21:01:57 +0300

    spelling

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,9 +3,9 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>MindWaves is an application that offers as solutions to mindfulness learning
-        through the use of AI, data represenation and interactive rubrics that help teachers pinpoint every students
-        stregths, weaknesses and insecurities.
+      <Text style={styles.text}>MindWaves is an application that offers solutions to mindfulness learning
+        through the use of AI, data representation and interactive rubrics that help teachers pinpoint every student's
+        strengths, weaknesses and insecurities.
       </Text>
     </View>
   );
